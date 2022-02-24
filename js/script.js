45aa0872e30a3c704883592965d0c8daca5c105a
cmd-source <conorcrosbie09@gmail.com>
2022-02-24 17:18:25 +0000

    logging keywords

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
 
+    const keys = document.querySelectorAll(".keyboard-row button");
+
+    for (let i = 0; i < keys.length; i++) {
+        keys[i].onclick = ({ target }) => {
+            const letter = target.getAttribute("data-key");
+            console.log(letter)
+        };
+    }
+
     function createSquares() {
         const gameBoard = document.getElementById("board")
 
@@ -12,4 +21,8 @@ document.addEventListener("DOMContentLoaded", () => {
             gameBoard.appendChild(square);
         }
     }
+
+
+
+
 });
