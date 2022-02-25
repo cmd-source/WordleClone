87fcd2ea851f4ae382cba64488325309ff08d9d0
cmd-source <conorcrosbie09@gmail.com>
2022-02-25 12:19:08 +0000

    adding colors

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     createSquares();
-    window.alert("Weclome to Conor's Clone of Wordle")
+    //window.alert("Weclome to Conor's Clone of Wordle")
 
     let guessedWords = [[]]
     let availableSpace = 1;
-    let word = "Conor";
+    let word = "conor";
+    let guessedWordCount = 0;
 
     const keys = document.querySelectorAll(".keyboard-row button");
 
@@ -17,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 handleSubmit()
                 return;
             }
+
         };
     }
 
@@ -38,11 +40,59 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    function getTitleColor(letter, index) {
+        const isCorrectLetter = word.includes(letter)
+
+        if(!isCorrectLetter) {
+            return "rgb(58,58,60)"
+        }
+
+        const letterInThatPosition = word.charAt(index)
+        const isCorrectPosition = letter === letterInThatPosition
+
+        if (isCorrectPosition) {
+            return "rgb(83,141,78)"
+        } else {
+            return "rgb(181, 158, 59)";
+        }
+    }
+
+
     function handleSubmit() {
         const currentWordArr = getCurrentWordArr()
         if (currentWordArr.length !==5 ) {
             window.alert("word must be 5 letters")
         }
+        
+
+        const currentWord = currentWordArr.join("")
+        const firstLetterId = guessedWordCount * 5 + 1;
+        const interval = 200
+        currentWordArr.forEach((letter, index) => {
+            setTimeout(() => {
+                const tileColor = getTitleColor(letter, index)
+                const letterId = firstLetterId + index;
+                const letterEl = document.getElementById(letterId)
+                letterEl.classList.add("animate___flipInX")
+                letterEl.style = `background-color:${tileColor}; border-color: ${tileColor}`
+            }, interval * index)
+        })
+
+        guessedWordCount += 1
+
+        if ( currentWord === word) {
+            $( function() {
+                $( "#overlay" ).fadeIn();
+              }, 2000 );
+            window.alert("Congradulations you won a Conor")
+        }
+
+        if(guessedWords.length === 6) {
+            window.alert(`Sorry the word was ${word}`)
+        }
+
+        guessedWords.push([])
+
     }
 
     function createSquares() {
@@ -53,11 +103,13 @@ document.addEventListener("DOMContentLoaded", () => {
             let square = document.createElement("div");
             square.classList.add("square");
             square.setAttribute("id", index + 1);
+            square.classList.add("animate__animated")
             gameBoard.appendChild(square);
         }
     }
 
-
-
+    $( function() {
+        $( "#overlay" ).hide();
+      } );
 
 });
